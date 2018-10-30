2ba588bd75647e1c46798948b5b417a87181121b
arch-mac <manuel@sahores.com>
2018-10-30 16:57:26 -0300

    one

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    input: ""
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          value={this.state.input}
+          onChange={this.changeInputHandler.bind(this)}
+        />
+        <p>{this.state.input}</p>
       </div>
     );
+  }
+
+  changeInputHandler(evt) {
+    this.setState({ input: evt.target.value });
   }
 }
 
