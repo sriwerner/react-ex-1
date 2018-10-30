e9865976bdfe00e1c0ff4ecb8581bc3e250b99c4
arch-mac <manuel@sahores.com>
2018-10-30 17:10:21 -0300

    third step

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const validationComponent = props => {
-  let size = props.size;
-  return <p>{size}</p>;
+  let message;
+  if (props.size < 5) {
+    message = "text too short";
+  } else {
+    message = "text long enough";
+  }
+  return <p>{message}</p>;
 };
 
 export default validationComponent;
